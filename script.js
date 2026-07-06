9ed62ca0e992b710b63c802288a5e19c7785404b
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-06 18:14:57 +0530

    Added dark mode background

--- script.js
+++ script.js
@@ -58,3 +58,11 @@ topBtn.addEventListener("click", () => {
     });
 
 });
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("bg-gray-900");
+    document.body.classList.toggle("text-white");
+
+});
